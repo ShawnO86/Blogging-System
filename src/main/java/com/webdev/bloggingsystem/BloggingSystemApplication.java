ddc702e0d7df9b20badcb3e604b40a9572c82bad
shawn <shawnby222@gmail.com>
2025-10-08 13:23:25 -0500

    Init

---
+++ src/main/java/com/webdev/bloggingsystem/BloggingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.bloggingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingSystemApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BloggingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webdev/bloggingsystem/BloggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.bloggingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
