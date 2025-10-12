524d5ca9f5a333723afed73877f7e0c3c4dcff15
shawn <shawnby222@gmail.com>
2025-10-12 16:33:34 -0500

    Adds data and config for test env

---
+++ src/test/resources/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO Roles(role)
+VALUES
+    ('USER'),
+    ('ADMIN');
+
+INSERT INTO Users(username, password, email, date_created, is_active)
+VALUES
+    ('TestAdmin', 'TestPassword', 'TestEmail@email.com', '2025-10-05', TRUE),
+    ('TestUser', 'TestPassword', 'TestEmail@email.com', '2025-10-05', TRUE);
+
+INSERT INTO Users_Roles(user_id, role_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1);
+
+INSERT INTO Categories(category, description)
+VALUES
+    ('Test Category', 'Just a test category.');
+
+INSERT INTO Blog_Entries(title, date_published, content, is_public, author_id)
+VALUES
+    ('Test Post 1', '2025-09-30', 'Test Post 1 - TestAdmin content is here.',TRUE, 1),
+    ('Test Post 2', '2025-10-02', 'Test Post 2 - TestAdmin content is here.',TRUE, 1),
+    ('Test Post 3', '2025-10-03', 'Test Post 3 - TestUser content is here.',TRUE, 2);
+
+INSERT INTO Posts_Categories(post_id, category_id)
+VALUES
+    (1,1),
+    (2,1),
+    (3,1);
+
+INSERT INTO Comments(content, date_created, parent_comment_id, author_id, post_id)
+VALUES
+    ('Test Comment on Test Post 1', '2025-10-01', null, 1, 1),
+    ('Test Reply to Comment 1 on Test Post 1', '2025-10-01', 1, 2, 1);
