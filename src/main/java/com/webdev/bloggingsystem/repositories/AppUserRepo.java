2b5db6e1f2a4d7e60cbd1853e044676baf325176
shawn <shawnby222@gmail.com>
2025-10-17 16:13:36 -0500

    Adds findByUsername method

--- src/main/java/com/webdev/bloggingsystem/repositories/AppUserRepo.java
+++ src/main/java/com/webdev/bloggingsystem/repositories/AppUserRepo.java
@@ -4,4 +4,5 @@ import com.webdev.bloggingsystem.entities.AppUser;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AppUserRepo extends CrudRepository<AppUser, Integer> {
+    AppUser findByUsername(String username);
 }
